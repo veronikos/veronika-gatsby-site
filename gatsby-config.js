5dfb5dc23ffe7756f4ee0e959d4cecfc2aee06b3
Veronika_Kostenko_R <veronikostenko@gmail.com>
2021-07-09 12:26:59 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Veronika Gatsby Site",
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-gatsby-cloud"],
+};
