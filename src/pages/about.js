f5450bde5ac29de59632498d8577fdd281ac0c6b
Veronika_Kostenko_R <veronikostenko@gmail.com>
2021-07-13 14:27:35 +0300

    add custom pages

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+// Step 1: Import your component
+import * as React from 'react'
+import Layout from "../components/layout";
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+      <Layout pageTitle="About">
+    <main>
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </main>
+      </Layout>
+
+  )
+}
+
+// Step 3: Export your component
+export default AboutPage
